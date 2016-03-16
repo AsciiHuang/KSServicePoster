70824fe3617814c9e171a136d8d9e9abdc9eec2c
AsciiHuang <asciihuang@kkbox.com>
2016-03-17 00:27:00 +0800

    temp

--- KSServicePoster/MainWindow.xaml.cs
+++ KSServicePoster/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,71 @@ namespace KSServicePoster
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private ObservableCollection<String> itemsSource = new ObservableCollection<String>();
+        public ObservableCollection<String> ItemsSource
+        {
+            get
+            {
+                return itemsSource;
+            }
+            set
+            {
+                itemsSource = value;
+                NotifyPropertyChanged("ItemsSource");
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //tlMedia.Source = new Uri("C:\\Users\\Ascii\\Downloads\\12.gif", UriKind.Absolute);
+            //imgMedia.Source = new BitmapImage(new Uri("C:\\Users\\Ascii\\Downloads\\12.gif", UriKind.Absolute));
+        }
+
+        private void OnAddItemButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".ksd";
+            dlg.Filter = "KSService Files (*.ksd)|*.ksd";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                itemsSource.Add(dlg.FileName);
+                NotifyPropertyChanged("ItemsSource");
+            }
+        }
+
+        private void OnDeleteItemButtonClicked(object sender, RoutedEventArgs e)
+        {
+            int currentIndex = listViewContent.SelectedIndex;
+            if (currentIndex != -1)
+            {
+                itemsSource.RemoveAt(currentIndex);
+                NotifyPropertyChanged("ItemsSource");
+            }
+        }
+
+        private void OnStartRunButtonClicked(object sender, RoutedEventArgs e)
+        {
+            new Poster(itemsSource).Show();
+            this.Close();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
